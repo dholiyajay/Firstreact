221d756c8ae53bb7ae3f978a269d0b3feddacb5f
dholiyajay <dholiyajay70@gmail.com>
2024-03-11 08:02:20 +0530

    home

--- src/user/container/producat/Producat.js
+++ src/user/container/producat/Producat.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
 import './Producat.module.css';
+import { ThreeDots } from 'react-loader-spinner';
 
 function Producat(props) {
     const [producatdata, setproducatdata] = useState([]);
@@ -20,11 +21,11 @@ function Producat(props) {
         const data = await respons.json();
         setproducatdata(data);
 
-        
+
         const uinque = [];
 
-        setloading(true,9000);
-        
+        setloading(true);
+
 
         data.map((v, i) => {
             if (!uinque.includes(v.category)) {
@@ -64,54 +65,70 @@ function Producat(props) {
 
     const finaldata = searchData();
 
-    
+
 
     return (
         <div className="container">
-          {
-            loading === false ? <h1>Loading...</h1> : ''
-          }
-            <input type="text" placeholder="Search" id="search" onChange={(event) => Setproduct(event.target.value)} />
-            <select onChange={(event) => setselect(event.target.value)}>
-                <option value="0">select</option>
-                <option value="hw">high to low</option>
-                <option value="lh">low to high</option>
-                <option value="az">a to z</option>
-                <option value="za">z to a</option>
-            </select>
+            {
+                loading === false ? <>
+                    <ThreeDots
+                        visible={true}
+                        height="80"
+                        width="80"
+                        color="#4fa94d"
+                        radius="9"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />
+                </> : (
+                    <>
 
-            <div className="row">
-                <div>
-                    <ul>
-                        <button style={{ backgroundColor: data === '' ? 'red' : 'yellow' }} onClick={() => setdata('')}>All</button>
-                        {Categories.map((v, i) => (
-                            <button style={{ backgroundColor: data === v ? 'red' : 'yellow' }} onClick={() => setdata(v)} key={i}>{v}</button>
-                        ))}
-                    </ul>
-                </div>
+                        <input type="text" placeholder="Search" id="search" onChange={(event) => Setproduct(event.target.value)} />
+                        <select onChange={(event) => setselect(event.target.value)}>
+                            <option value="0">select</option>
+                            <option value="hw">high to low</option>
+                            <option value="lh">low to high</option>
+                            <option value="az">a to z</option>
+                            <option value="za">z to a</option>
+                        </select>
 
-                {finaldata.map((v, i) => (
-                    <div className="col-md-4 gy-5" key={i}>
-                        <Card style={{ width: '18rem' }}>
-                            <img alt="Sample" style={{ height: '350px' }} src={v.image} />
-                            <CardBody>
-                                <CardTitle tag="h5">
-                                    {v.title.length > 10 ? v.title.substring(0, 20) + '...' : v.title}
-                                </CardTitle>
-                                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                                    $ {v.price}
-                                </CardSubtitle>
-                                <CardText>{v.category}</CardText>
-                                <CardText>
-                                    {v.description.length > 50 ? v.description.substring(0, 50) + '...' : v.description}
-                                </CardText>
-                                <Button>Add to Cart</Button>
-                            </CardBody>
-                        </Card>
-                    </div>
-                ))}
-            </div>
+                        <div className="row">
+                            <div>
+                                <ul>
+                                    <button style={{ backgroundColor: data === '' ? 'red' : 'yellow' }} onClick={() => setdata('')}>All</button>
+                                    {Categories.map((v, i) => (
+                                        <button style={{ backgroundColor: data === v ? 'red' : 'yellow' }} onClick={() => setdata(v)} key={i}>{v}</button>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            {finaldata.map((v, i) => (
+                                <div className="col-md-4 gy-5" key={i}>
+                                    <Card style={{ width: '18rem' }}>
+                                        <img alt="Sample" style={{ height: '350px' }} src={v.image} />
+                                        <CardBody>
+                                            <CardTitle tag="h5">
+                                                {v.title.length > 10 ? v.title.substring(0, 20) + '...' : v.title}
+                                            </CardTitle>
+                                            <CardSubtitle className="mb-2 text-muted" tag="h6">
+                                                $ {v.price}
+                                            </CardSubtitle>
+                                            <CardText>{v.category}</CardText>
+                                            <CardText>
+                                                {v.description.length > 50 ? v.description.substring(0, 50) + '...' : v.description}
+                                            </CardText>
+                                            <Button>Add to Cart</Button>
+                                        </CardBody>
+                                    </Card>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                )
+            }
         </div>
+
     );
 }
 
